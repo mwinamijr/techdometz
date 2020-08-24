83864e22919d9b9725f753f03eeff7f425e86a4a
mwinamijr <mwinamijr@gmail.com>
2020-08-24 17:33:03 +0300

    added routes.js

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is runnin on port ${port}`)
+})
+
+const routes = require('./routes/home');
+
+app.use('/', routes);
