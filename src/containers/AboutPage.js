a171215458f66a1f87c9fde058c1fb20273d4d83
mwinamijr <mwinamijr@gmail.com>
2020-10-07 19:31:03 +0300

    Changes contents of the about page

--- src/containers/AboutPage.js
+++ src/containers/AboutPage.js
@@ -1,23 +1,97 @@
-import React from 'react';
-
-import { Container, Col, Row } from 'react-bootstrap';
+import React from "react";
+import {  MDBRow, MDBCol, MDBIcon } from "mdbreact";
 
 const AboutPage = () => {
-	return (
-			<div>
-				<Container>
-				  <Row>
-				    <Col sm={8}>sm=8</Col>
-				    <Col sm={4}>sm=4</Col>
-				  </Row>
-				  <Row>
-				    <Col sm>sm=true</Col>
-				    <Col sm>sm=true</Col>
-				    <Col sm>sm=true</Col>
-				  </Row>
-				</Container>
-			</div>
-		)
+  return (
+      <section className="text-center my-5">
+        <h2 className="h1-responsive font-weight-bold my-5">
+          Why is it so great?
+        </h2>
+        <p className="lead grey-text w-responsive mx-auto mb-5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          ad minim veniam.
+        </p>
+
+				<MDBRow>
+          <MDBCol lg="5" className="text-center text-lg-left">
+            <img
+              className="img-fluid"
+              src="https://mdbootstrap.com/img/Photos/Others/screens-section.jpg"
+              alt=""
+            />
+          </MDBCol>
+          <MDBCol lg="7">
+            <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="share" size="lg" className="indigo-text" />
+              </MDBCol>
+              <MDBCol xl="10" md="11" size="10">
+                <h5 className="font-weight-bold mb-3">Safety</h5>
+                <p className="grey-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit
+                  enim ad minima veniam, quis nostrum exercitationem ullam.
+                  Reprehenderit maiores aperiam assumenda deleniti hic.
+                </p>
+              </MDBCol>
+            </MDBRow>
+            <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="share" size="lg" className="indigo-text" />
+              </MDBCol>
+              <MDBCol xl="10" md="11" size="10">
+                <h5 className="font-weight-bold mb-3">Technology</h5>
+                <p className="grey-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit
+                  enim ad minima veniam, quis nostrum exercitationem ullam.
+                  Reprehenderit maiores aperiam assumenda deleniti hic.
+                </p>
+              </MDBCol>
+            </MDBRow>
+            <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="share" size="lg" className="indigo-text" />
+              </MDBCol>
+              <MDBCol xl="10" md="11" size="10">
+                <h5 className="font-weight-bold mb-3">Finance</h5>
+                <p className="grey-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit
+                  enim ad minima veniam, quis nostrum exercitationem ullam.
+                  Reprehenderit maiores aperiam assumenda deleniti hic.
+                </p>
+              </MDBCol>
+            </MDBRow>
+          </MDBCol>
+        </MDBRow>
+				<br />
+        <MDBRow>
+          <MDBCol md="4">
+            <MDBIcon icon="chart-area" size="3x" className="red-text" />
+            <h5 className="font-weight-bold my-4">Analytics</h5>
+            <p className="grey-text mb-md-0 mb-5">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Reprehenderit maiores aperiam minima assumenda deleniti hic.
+            </p>
+          </MDBCol>
+          <MDBCol md="4">
+            <MDBIcon icon="book" size="3x" className="cyan-text" />
+            <h5 className="font-weight-bold my-4">Tutorials</h5>
+            <p className="grey-text mb-md-0 mb-5">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Reprehenderit maiores aperiam minima assumenda deleniti hic.
+            </p>
+          </MDBCol>
+          <MDBCol md="4">
+            <MDBIcon far icon="comments" size="3x" className="orange-text" />
+            <h5 className="font-weight-bold my-4">Support</h5>
+            <p className="grey-text mb-md-0 mb-5">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Reprehenderit maiores aperiam minima assumenda deleniti hic.
+            </p>
+          </MDBCol>
+        </MDBRow>
+      </section>
+  );
 }
 
 export default AboutPage;
