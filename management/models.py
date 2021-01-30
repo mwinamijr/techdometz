f17075965e5e7181d7015c6cb2102af34b9ee74c
mwinamijr <mwinamijr@gmail.com>
2021-01-30 08:38:24 +0300

    modified carousel model

--- management/models.py
+++ management/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Carousel(models.Model):
     title = models.CharField(max_length=100, blank=True, null=True)
     description = models.TextField()
-    image = models.ImageField(name="carousels", to="carousel")
+    image = models.ImageField(name="carousels", upload_to="carousel")
 
     def __str__(self):
         return self.title
