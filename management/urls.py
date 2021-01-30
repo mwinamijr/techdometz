df3f04eef7460e88810c3fc6affb75a1f6a05a95
mwinamijr <mwinamijr@gmail.com>
2021-01-30 09:38:36 +0300

    created urls for carousel views

---
+++ management/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import CarouselList, CarouselDetail
+
+urlpatterns = [
+    path('carousel/', CarouselList.as_view()),
+    path('carousel/<int:pk>/', CarouselDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
