a73712f078321e86c9ce896ad5b63d7cabe20af8
Athumani Mwinami <mwinamijr@gmail.com>
2021-08-19 14:39:05 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from "react-bootstrap";
 
