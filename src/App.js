a53ed51fc81ca1154a3cf027a2afb5811959e59c
mwinamijr <mwinamijr@gmail.com>
2024-02-17 17:50:00 +0300

    created new updated react app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
-import React from 'react';
-import { HashRouter as Router, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from "react-bootstrap";
-
-import About from './containers/AboutPage';
-import Developers from './containers/Developers';
-import Services from './containers/Services';
-import Home from './containers/HomePage';
-import Contact from './components/ContactForm';
-import Header from './components/Navbar';
-import Footer from './components/Footer';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  
-
   return (
-    <Router>
-      <Header />
-      <main className="py-3 lead">
-        <Container>
-          <Route path='/' component={Home} exact /> {}
-          <Route path='/services' component={Services} /> {}
-          <Route path='/about' component={About} /> {}
-          <Route path='/developers' component={Developers} /> {}
-          <Route path='/contact' component={Contact} /> {}
-        </Container>
-      </main>
-      <Footer />
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
